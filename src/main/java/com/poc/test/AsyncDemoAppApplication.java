d75be3d3ecb7ef1c81b7988852d40ddbdf1df488
Souvik Dasgupta <dg.souvik@gmail.com>
2025-09-14 11:50:13 +0530

    first commit
    First commit

---
+++ src/main/java/com/poc/test/AsyncDemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.poc.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncDemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncDemoAppApplication.class, args);
+	}
+
+}
