c64a217e8e9d0caec567f35fea97607f1ba1252d
Souvik Dasgupta <dg.souvik@gmail.com>
2025-09-14 12:05:24 +0530

    Changes made

---
+++ src/main/java/com/poc/test/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.poc.test.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+	
+	@GetMapping("/sayHello")
+	public String hello() {
+		return "Hello World";
+	}
+
+}
